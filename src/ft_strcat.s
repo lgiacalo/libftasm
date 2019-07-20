41a12ad323b0726a078cf4d5f141f79d1a090043
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2019-07-20 19:51:35 +0200

    ft_strcat.s

---
+++ src/ft_strcat.s
@@ -0,0 +1,31 @@
+global _ft_strcat
+
+section .text
+
+_ft_strcat:
+push	rbp
+mov		rbp, rsp
+mov		rax, rdi
+;cmp		rdi, 0x0
+;je		_leave
+;cmp		rsi, 0x0
+;je		_leave
+
+_loop:
+cmp		byte[rdi], 0x0
+je		_copy
+inc		rdi
+jmp		_loop
+
+_copy:
+mov		r8b, byte[rsi]
+mov		byte[rdi], r8b
+cmp		byte[rsi], 0x0
+je		_leave
+inc		rdi
+inc		rsi
+jmp	 	_copy
+
+_leave:
+leave
+ret
