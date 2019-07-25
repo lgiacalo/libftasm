4aa878d0c9c5e79130b783986bdbdb0571f405fb
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2019-07-26 01:23:15 +0200

    UPD: ft_bzero OKOK

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -1,28 +1,40 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_bzero.s                                         :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lgiacalo <marvin@42.fr>                    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/06/20 15:53:32 by lgiacalo          #+#    #+#              #
-#    Updated: 2019/06/20 16:28:42 by lgiacalo         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 global _ft_bzero
 
 section .text
 
-_ft_bzero:
-	cmp		rsi, 0x0			; check rsi value == 0
-	je		_leave				; jump rsi == 0
-	cmp		rdi, 0x0			; check string non null
-	je		_leave				; jump rdi == NULL
-	mov		[rdi], byte 0x0		; mov rdi[] <- 0x0 == 0 ==  \0
-	dec		rsi					; decrementation rsi
-	inc		rdi					; incrementation rdi
-	jmp		_ft_bzero			; recursivite _ft_bzero
-
 _leave:
-	ret							; return
+	mov		rsp, rbp
+	pop		rbp
+	ret
+
+_ft_bzero:
+	push	rbp
+	mov		rbp, rsp
+
+	cmp		rsi, 0x0
+	je		_leave
+	cmp		rdi, 0x0
+	je		_leave
+
+_loop:
+	mov		[rdi], byte 0x0
+	dec		rsi
+	inc		rdi
+	cmp		rsi, 0x0
+	je		_leave
+	jmp		_loop
+
+
+
+; _ft_bzero:
+; 	cmp		rsi, 0x0			; check rsi value == 0
+; 	je		_leave				; jump rsi == 0
+; 	cmp		rdi, 0x0			; check string non null
+; 	je		_leave				; jump rdi == NULL
+; 	mov		[rdi], byte 0x0		; mov rdi[] <- 0x0 == 0 ==  \0
+; 	dec		rsi					; decrementation rsi
+; 	inc		rdi					; incrementation rdi
+; 	jmp		_ft_bzero			; recursivite _ft_bzero
+;
+; _leave:
+; 	ret							; return
