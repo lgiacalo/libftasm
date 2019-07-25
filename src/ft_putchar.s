d536a4bcb77d7ce6af8734ea50dc27471b0b94ef
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2019-07-26 01:46:54 +0200

    UPD: ft_putchar OKOK

--- src/ft_putchar.s
+++ src/ft_putchar.s
@@ -6,16 +6,13 @@ _leave:
 	leave
 	ret
 
-
 _ft_putchar:
 	push	rbp
 	mov		rbp, rsp
 	sub		rsp, 16
 
-
 	mov		byte [rsp], dil
 	mov		rsi, rsp
-	; lea		rsi, [rsp]
 
 	mov     rax, 0x2000004
 	mov     rdi, 1			; fd
