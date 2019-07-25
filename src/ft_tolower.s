8a1b6ce000fd9a17401a9c6f10063d56902a0b40
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2019-07-26 01:52:02 +0200

    UPD: ft_tolower OKOK

--- src/ft_tolower.s
+++ src/ft_tolower.s
@@ -8,17 +8,20 @@ _leave:
 	ret
 
 _istrue:
+	pop		rdi
 	add		rdi, 32
 	mov		rax, rdi
 	jmp		_leave
 
 _isfalse:
+	pop		rdi
 	mov		rax, rdi
 	jmp		_leave
 
 _ft_tolower:
 	push	rbp
 	mov		rbp, rsp
+	push	rdi
 
 	call	_ft_isupper
 	cmp		rax, 0
